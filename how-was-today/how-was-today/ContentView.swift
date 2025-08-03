3934a76a2a638dd3cf00a6722ad41624a7eb76f0
hogang <hyogangster1@gmail.com>
2025-08-03 13:22:35 +0900

    create project

---
+++ how-was-today/how-was-today/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  how-was-today
+//
+//  Created by hogang on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ how-was-today/how-was-today/how_was_todayApp.swift
@@ -0,0 +1,17 @@
+//
+//  how_was_todayApp.swift
+//  how-was-today
+//
+//  Created by hogang on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct how_was_todayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
