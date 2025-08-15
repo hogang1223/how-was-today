eee03cb7425dedd0a8f7ce5b37d0c1c243c88ca8
hogang <hyogangster1@gmail.com>
2025-08-15 12:03:02 +0900

    feat - DailySupplementLogRepository 구현

---
+++ how-was-today/how-was-today/Presentation/Features/TodaySummary/ViewModels/TodaySummaryViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  TodaySummaryViewModel.swift
+//  how-was-today
+//
+//  Created by hogang on 8/15/25.
+//
+
+import Foundation
+
+final class TotalSummaryViewModel: ObservableObject {
+    
+    @Published var supplement: Supplement
+    
+    private let supplementRepo: SupplementPlanRepository
+    
+    init(supplementRepo: SupplementPlanRepository) {
+        self.supplementRepo = supplementRepo
+        
+        self.supplement = Supplement(date: Date(), names: [])
+        
+        loadSupplement()
+    }
+    
+    
+    private func loadSupplement() {
+        
+        
+        
+    }
+}
